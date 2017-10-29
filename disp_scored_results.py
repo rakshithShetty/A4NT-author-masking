d43f3f512f0b230b1c8381fa501bb344cf941014
Rakshith Shetty <rshetty@mpi-inf.mpg.de>
2017-10-29 21:43:46 +0100

    filter by change

--- disp_scored_results.py
+++ disp_scored_results.py
@@ -71,26 +71,29 @@ def main(params):
                 st, m_idx = get_sentence(st,params['filter_score'], params['filter_by'])
 
             inpset = set(st['sent'].split()[:-1])
-            if len(inpset) > 0 and st['score'][1-ix]>params['filter']:
+            if len(inpset) > 0 and (st['score'][1-ix]>params['filter'][0]) and (st['score'][1-ix]<params['filter'][1]) and (params['lenfilter']==0 or (len(inpset) < params['lenfilter'])):
                 genset = set(st['trans'].split()[:-1])
-                recall[ix].append(float(len(inpset & genset))/float(len(inpset)))
-                meteor_score[ix].append(st[params['filter_score']])
-                sents[ix].append(st['sent'])
-                sc[ix].append(st['score'][1-ix])
-                trans_sents[ix].append(st['trans'])
-                if params['show_samples']:
-                    trans_sents_other[ix].append([ost['trans'] for osti,ost in enumerate(doc['sents'][j]) if osti!=m_idx])
-                if params['otherres']:
-                    for oi in xrange(len(ores)):
-                        ost, _ = get_sentence(ores[oi]['docs'][i]['dict_sents'][st['sid']],params['filter_score'], params['other_filter_by'])
-                        other_trans_sents[oi][ix].append(ost['trans'])
-                diff_sc[ix].append(st['trans_score'][ix] - st['score'][ix])
-                if params['semantic_embedding']:
-                    emb_diff[ix].append(np.abs(sem_emb[st['trans_enc'],:] - sem_emb[st['sent_enc'],:]).sum())
+                delta = len(inpset - genset) + len(genset-inpset )
+                if params['deltafilter'] < 0 or (delta == params['deltafilter']):
+                    recall[ix].append(float(len(inpset & genset))/float(len(inpset)))
+                    meteor_score[ix].append(st[params['filter_score']])
+                    sents[ix].append(st['sent'])
+                    sc[ix].append(st['score'][1-ix])
+                    trans_sents[ix].append(st['trans'])
+                    if params['show_samples']:
+                        trans_sents_other[ix].append([ost['trans'] for osti,ost in enumerate(doc['sents'][j]) if osti!=m_idx])
+                    if params['otherres']:
+                        for oi in xrange(len(ores)):
+                            ost, _ = get_sentence(ores[oi]['docs'][i]['dict_sents'][st['sid']],params['filter_score'], params['other_filter_by'])
+                            other_trans_sents[oi][ix].append(ost['trans'])
+                    diff_sc[ix].append(st['trans_score'][1-ix] - st['score'][1-ix])
+                    if params['semantic_embedding']:
+                        emb_diff[ix].append(np.abs(sem_emb[st['trans_enc'],:] - sem_emb[st['sent_enc'],:]).sum())
 
     diff_sc = [np.array(diff_sc[0]), np.array(diff_sc[1])]
     recall = [np.array(rc) for rc in recall]
     meteor = [np.array(mt) for mt in meteor_score]
+    sc = [np.array(s) for s in sc]
     if params['semantic_embedding']:
         emb_diff = [np.array(emb) for emb in emb_diff]
     for j in xrange(2):
@@ -98,13 +101,14 @@ def main(params):
         print 'Author %s to %s translation'%(ix_to_auth[j], ix_to_auth[1-j])
         print '----------------------------------------------------------'
         #score = diff_sc[j] * meteor[j]
+        score = -(((diff_sc[j] + sc[j])>0.5).astype(float) * meteor[j])
         #score = np.random.rand(len(recall[j]))#-(1-recall[j]) * meteor[j]
-        score = diff_sc[j]#-(1-recall[j]) * meteor[j]
+        #score = diff_sc[j]#-(1-recall[j]) * meteor[j]
         for i in (score).argsort()[params['offset']:params['offset']+params['ndisp']]:
             if params['semantic_embedding']:
                 print 'diff %.2f, recall: %.2f, emb_diff: %.2f'%(-diff_sc[j][i],recall[j][i], emb_diff[j][i])
             else:
-                print 'diff %.2f, recall: %.2f, meteor: %.2f, orig: %.2f'%(-diff_sc[j][i],recall[j][i], meteor[j][i], sc[j][i])
+                print 'diff %.2f, recall: %.2f, meteor: %.2f, orig: %.2f'%(diff_sc[j][i],recall[j][i], meteor[j][i], sc[j][i])
             #print colored('Inp %6s -->'%(ix_to_auth[j]),'green') + colored(' %s'%(sents[j][i]), auth_colors[j])
             #print colored('Out %6s -->'%(ix_to_auth[1-j]),'grey') + colored(' %s'%(trans_sents[j][i]), auth_colors[1-j])
             #if params['otherres']:
@@ -130,7 +134,9 @@ if __name__ == "__main__":
   parser.add_argument('-a','--age', dest='age', type=int, default=1, help='batch_size to use')
   parser.add_argument('-o','--offset', dest='offset', type=int, default=0, help='batch_size to use')
   parser.add_argument('-s','--semantic_embedding', dest='semantic_embedding', type=str, default=None, help='batch_size to use')
-  parser.add_argument('-f','--filter', dest='filter', type=float, default=0., help='batch_size to use')
+  parser.add_argument('-f','--filter', dest='filter', type=float, nargs='+', default=0., help='batch_size to use')
+  parser.add_argument('-d','--deltafilter', dest='deltafilter', type=int, default=-1, help='batch_size to use')
+  parser.add_argument('-l','--lenfilter', dest='lenfilter', type=float, default=0., help='batch_size to use')
   parser.add_argument('--filter_score', type=str, default='METEOR',help='the input candidateJson')
   parser.add_argument('--filter_by', type=str, default='min',help='the input candidateJson')
   parser.add_argument('--show_samples', type=int, default=0,help='Show all samples of the primary model')
